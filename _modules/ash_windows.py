e7a3dff83db639c1eb18b772e434f8bf53b9fab9
lorengordon <loren@fleet-it.com>
2014-10-16 07:11:59 -0400

    updated ash_windows.py module and .gitignore

--- _modules/ash_windows.py
+++ _modules/ash_windows.py
@@ -1,6 +1,8 @@
 import fnmatch
 
-def filter_by(lookup_table, match): 
-    for key in lookup_table.keys(): 
-        if fnmatch.fnmatch(match, key): 
-            return lookup_table[key] 
+
+def filter_by(lookup_table, match):
+    for key in lookup_table.keys():
+        if fnmatch.fnmatch(match, key):
+            return lookup_table[key]
+    return None
