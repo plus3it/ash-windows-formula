2b850bc9e7d46b2c6fc696eed420e6bc0e8a46f9
Loren Gordon <lorengordon@users.noreply.github.com>
2020-03-02 09:56:00 -0800

    Deletes unused custom execution module

--- _modules/ash_windows.py
+++
@@ -1,8 +0,0 @@
-import fnmatch
-
-
-def filter_by(lookup_table, match):
-    for key in lookup_table.keys():
-        if fnmatch.fnmatch(match, key):
-            return lookup_table[key]
-    return None
