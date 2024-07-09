91c729dcd3bcd5ec6639471684d42ea2835aaf30
Long Lam <31355535+eemperor@users.noreply.github.com>
2024-07-09 15:48:07 -0400

    Adds MULTISZ registry type as an option in conversion script

--- ash-windows/tools/convert-lgpo-policy.py
+++ ash-windows/tools/convert-lgpo-policy.py
@@ -9,7 +9,7 @@ import yaml
 REG_CODE_MAP = {"1": "SZ", "2": "EXSZ", "3": "BINARY", "4": "DWORD", "7": "MULTISZ"}
 REG_MODES = ("DELETE", "DELETEALLVALUES", "CREATEKEY")
 REG_HIVES = ("USER", "COMPUTER")
-REG_TYPES = ("DWORD", "SZ", "EXSZ")
+REG_TYPES = ("DWORD", "SZ", "EXSZ", "MULTISZ")
 
 
 def _convert_regpol(src):
